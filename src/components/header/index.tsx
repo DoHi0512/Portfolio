2e89291baa4d874bb5ec5454be1175e9daca9177
DoHi0512 <donghuni512@gmail.com>
2023-03-16 20:29:21 +0900

    Create:: Header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,9 @@
+import * as S from "./style";
+export const Header = () => {
+  return (
+    <S.HeaderLayout>
+      <S.HeaderNav></S.HeaderNav>
+      <S.ProgressBar />
+    </S.HeaderLayout>
+  );
+};
---
+++ src/components/header/style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+export const HeaderLayout = styled.div`
+  height: 4rem;
+  width: 100%;
+  position: fixed;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const HeaderNav = styled.div`
+  width: 100%;
+  height: 3.5rem;
+`;
+export const ProgressBar = styled.div`
+  width: 100%;
+  height: 0.5rem;
+  border: 1px solid;
+`;
