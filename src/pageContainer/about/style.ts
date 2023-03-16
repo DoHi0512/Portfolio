6ce8f7b37514cb04024392050b4ddb8aee252ba7
DoHi0512 <donghuni512@gmail.com>
2023-03-16 20:29:38 +0900

    Create:: About page

---
+++ src/pageContainer/about/style.ts
@@ -0,0 +1,59 @@
+import tw from "tailwind-styled-components";
+
+export const AboutLayout = tw.div`
+bg-[#E8F8EC]
+w-screen
+h-screen
+flex
+justify-center
+relative
+items-center
+flex-col
+`;
+
+export const AboutBox = tw.div`
+w-[60rem]
+h-[30rem]
+border-[3px]
+border-[#A2D5C6]
+rounded-2xl
+flex
+justify-center
+items-center
+`;
+
+export const Title = tw.div`
+w-full
+h-[4rem]
+flex justify-center
+items-center
+text-5xl 
+text-[#4FA4A4A]
+`;
+export const Template = tw.div`
+
+`;
+export const ExpBox = tw.div`
+flex
+flex-row
+justify-center
+
+`;
+export const Explain = tw.div`
+text-2xl
+flex
+justify-center
+items-center
+`;
+export const Desc = tw.div`
+mt-2
+flex
+justify-center
+text-gray-600
+`;
+export const Icon = tw.div`
+flex
+justify-center 
+items-center
+mr-2
+`;
