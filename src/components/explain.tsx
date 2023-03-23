fb92564b4ea5f47056ab947a169548ff45ac354a
DoHi0512 <donghuni512@gmail.com>
2023-03-23 12:18:05 +0900

    Add:: image slider 추가

--- src/components/explain.tsx
+++ src/components/explain.tsx
@@ -4,6 +4,7 @@ import {
   AiOutlineGithub,
   AiOutlineYoutube,
 } from "react-icons/ai";
+import Slider from "react-slick";
 interface ContentProps {
   title: string;
   img: string;
@@ -13,6 +14,14 @@ interface ContentProps {
   github?: string;
   youtube?: string;
 }
+const settings = {
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: false,
+};
 export const Explain = ({
   title,
   desc,
@@ -29,10 +38,13 @@ export const Explain = ({
       </div>
       <div className="w-full h-full">
         <div className="w-full h-[70%] flex flex-row justify-between mt-2">
-          <img
-            src={img}
-            className="object-contain w-[25rem] h-[17rem] border-[1px] border-gray rounded-[5px]"
-          />
+          <Slider
+            className="[&>img]:object-contain w-[24rem] h-[17rem] border-[1px] border-gray rounded-[5px]"
+            {...settings}
+          >
+            <img src="./bmfo.png" />
+            <img src="./sciport.png" />
+          </Slider>
           <div className="w-[27rem]">
             <div className="h-[80%]">
               <div className="text-[1.25rem] font-semibold">{sentence}</div>
